31586a58966212ebe6297cdfce65cbd0f2727a0f
Vladyslav Rudkivskyi <v.rudkovskiy@gmail.com>
2019-02-14 01:35:01 +0200

    Added commit hook

--- scripts/git-pre-commit-hook.sh
+++ scripts/git-pre-commit-hook.sh
@@ -1,7 +1,13 @@
+#!/bin/sh
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT_ROOT_DIR=$SCRIPT_DIR/../..
 
 cd $PROJECT_ROOT_DIR
 
 LAST_COMMIT_HASH="$(git rev-parse HEAD)"
+
 lerna run tslint --since $LAST_COMMIT_HASH
+
+echo "*** Note that changes are done by 'tslint' are not added to a commit"
+echo "*** Look through them and append these changes to a commit using -->> 'git commit --amend [-a]' <<--"
--- scripts/install-hooks.sh
+++ scripts/install-hooks.sh
@@ -1,4 +1,4 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 GIT_HOOKS_DIR=$SCRIPT_DIR/../.git/hooks
 
-cp $SCRIPT_DIR/git-pre-commit-hook.sh $GIT_HOOKS_DIR/pre-commit.sh
+cp $SCRIPT_DIR/git-pre-commit-hook.sh $GIT_HOOKS_DIR/pre-commit
