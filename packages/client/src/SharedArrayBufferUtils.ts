0647bec3998362957252023073ca3761f5acbd24
Vladyslav Rudkivskyi <v.rudkovskiy@gmail.com>
2019-02-14 11:29:39 +0200

    Added Atomics usage

--- packages/client/src/SharedArrayBufferUtils.ts
+++ packages/client/src/SharedArrayBufferUtils.ts
@@ -5,7 +5,8 @@ export class SharedArrayBufferUtils {
 
     const strArr = [];
     for (let i = 0; i < length; i += 1) {
-      strArr[i] = String.fromCharCode(array[i + 1]);
+      const code = Atomics.load(array, i + 1);
+      strArr[i] = String.fromCharCode(code);
     }
     return strArr.join('');
   }
@@ -13,10 +14,13 @@ export class SharedArrayBufferUtils {
   public static stringToArray(str: string, array: Uint16Array) {
     const length = this.uint16(str.length);
 
-    array[0] = length;
+    // array[0] = length;
+    Atomics.store(array, 0, length);
+
     // 2 bytes for each char
     for (let i = 0, strLen = str.length; i < strLen; i += 1) {
-      array[i + 1] = str.charCodeAt(i);
+      // array[i + 1] = str.charCodeAt(i);
+      Atomics.store(array, i + 1, str.charCodeAt(i));
     }
   }
 
