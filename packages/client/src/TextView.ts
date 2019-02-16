76d95bb98f050bbb7e77b0e0a2ff1e4a850c60d0
Vladyslav Rudkivskyi <v.rudkovskiy@gmail.com>
2019-02-17 00:36:11 +0200

    WIP: Adding Vue.js to UI layer

--- packages/client/src/TextView.ts
+++ packages/client/src/TextView.ts
@@ -1,17 +1,17 @@
 export class TextView {
-  private elInputField: HTMLInputElement;
+  // private elInputField: HTMLInputElement;
 
   public constructor() {
-    this.elInputField = <HTMLInputElement> document.getElementsByClassName('input-field')[0];
+    // this.elInputField = <HTMLInputElement> document.getElementsByClassName('input-field')[0];
   }
 
   public subscribeOnTextChangeEvent(textChangeCallback: (value: string) => void) {
-    this.elInputField.addEventListener('input', () => {
-      textChangeCallback(this.elInputField.value || '');
-    });
+    // this.elInputField.addEventListener('input', () => {
+    //   textChangeCallback(this.elInputField.value || '');
+    // });
   }
 
   public setText(value: string): void {
-    this.elInputField.value = value;
+    // this.elInputField.value = value;
   }
 }
---
+++ packages/client/src/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <textarea v-model="text" class="input-field"></textarea>
+</template>
+
+<script lang="ts">
+  import { Vue, Component, Prop } from "vue-property-decorator";
+
+  @Component
+  export default class App extends Vue {
+    @Prop() text: string = '';
+  }
+</script>
+
+<style>
+  .input-field {
+    width: 600px;
+    height: 300px;
+}
+</style>
