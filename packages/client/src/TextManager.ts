e6ce25096c6f2c227360995d63f81383b73c56af
Vladyslav Rudkivskyi <v.rudkovskiy@gmail.com>
2019-02-17 02:15:09 +0200

    Added Vue.js to UI layer

--- packages/client/src/TextManager.ts
+++ packages/client/src/TextManager.ts
@@ -2,13 +2,15 @@ import { TextView } from './TextView';
 import { WebWorkerMessage, Action } from './WebWorkerMessage';
 import { WebWorkerClient } from './WebWorkerClient';
 
+type TextChangeCallback = (text: string) => void;
+
 export class TextManager {
 
-  public static create(view: TextView, host: string, port: number): Promise<TextManager> {
+  public static create(host: string, port: number): Promise<TextManager> {
 
     return WebWorkerClient.create('updateTextWorker.js', '')
       .then((client: WebWorkerClient) => {
-        const textManager = new TextManager(view, host, port, client);
+        const textManager = new TextManager(client);
 
         const connectMessage: WebWorkerMessage = WebWorkerMessage.create(Action.Connect,
                                                                          { host, port });
@@ -17,28 +19,26 @@ export class TextManager {
       });
   }
 
-  private view: TextView;
   private client: WebWorkerClient;
+  private textChangeListeners: TextChangeCallback[] = [];
 
-  private constructor(view: TextView,
-                      host: string,
-                      port: number,
-                      client: WebWorkerClient) {
-    this.view = view;
+  private constructor(client: WebWorkerClient) {
     this.client = client;
-
-    view.subscribeOnTextChangeEvent((text: string) => {
-      this.onTextChanged(text);
-    });
 
     this.client.subscribe((text: string) => {
       // TODO: Do not erase current text on view, but raise an error and disable sending diff.
       //       User have to reload page
-      this.view.setText(text);
+      this.textChangeListeners.forEach((callback: TextChangeCallback) => {
+        callback(text);
+      });
     });
   }
 
-  private onTextChanged(newText: string): void {
+  public subscribeToTextChanges(callback: TextChangeCallback) {
+    this.textChangeListeners.push(callback);
+  }
+
+  public onTextChanged(newText: string): void {
     this.client.updateText(newText)
       .catch(() => {
         console.error('Failed to send text update');
--- packages/client/src/app.vue
+++ packages/client/src/app.vue
@@ -1,13 +1,26 @@
 <template>
-  <textarea v-model="text" class="input-field"></textarea>
+  <textarea class="input-field" v-model="text"  v-on:input="onTextChanged()"></textarea>
 </template>
 
 <script lang="ts">
   import { Vue, Component, Prop } from "vue-property-decorator";
+  import { TextManager } from './TextManager';
 
   @Component
   export default class App extends Vue {
     @Prop() text: string = '';
+    textManager!: TextManager;
+
+    async mounted() {
+      this.textManager = await TextManager.create(window.location.hostname, 3000);
+      this.textManager.subscribeToTextChanges((text: string) => {
+        this.text = text;
+      });
+    }
+
+    onTextChanged() {
+      this.textManager.onTextChanged(this.text);
+    }
   }
 </script>
 
